9c2c6bdbd3173dae00b94b32890f325ca1b32e3b
Matheus Henrique Faiotto da Silva <matheus.faiotto@gmail.com>
2026-05-21 16:42:05 -0300

    Corrige timeout e headless

--- pages/GooglePage.ts
+++ pages/GooglePage.ts
@@ -1,15 +1,30 @@
-import { Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 
 export class GooglePage {
   constructor(private page: Page) {}
 
   async acessarGoogle() {
-    await this.page.goto('https://google.com');
+    await this.page.goto('https://www.google.com');
+
+    await this.page.waitForLoadState('networkidle');
   }
 
   async pesquisar(texto: string) {
-    await this.page.getByRole('combobox', { name: 'Pesquisar' }).fill(texto);
+    const campoPesquisa = this.page.getByRole('combobox');
+
+    await campoPesquisa.waitFor({
+      state: 'visible',
+      timeout: 30000
+    });
+
+    await campoPesquisa.click();
+
+    await campoPesquisa.fill(texto);
 
     await this.page.keyboard.press('Enter');
+
+    await this.page.waitForURL('**/search**', {
+      timeout: 30000
+    });
   }
 }
